bd4f3a969d6dcb48b8cc2a26aaa76c73586716de
Olga Vrou <olgavrou@gmail.com>
2022-05-01 21:22:29 -0400

    cleanup

--- gen_cat_libsvm_data.py
+++ gen_cat_libsvm_data.py
@@ -81,14 +81,3 @@ for i, outfile in enumerate(outputs):
             libsvm_data.write("\n")
     if 'train' in input_dirs[i]:
         print(f"TRADES nn model accuracy for cat class: {(float(acc)/float(total)) * 100}%")
-
-        
-
-# ./libsvm/svm-scale -s range libsvm_cat_train.dat > libsvm_cat_train.dat.scale 
-# ./libsvm/svm-scale -s range libsvm_cat_test.dat > libsvm_cat_test.dat.scale
-
-# run some hyper parameter tuning with libsvm using the same scripts and logic from homework 2 (https://github.com/olgavrou/FML-HW2)
-
-# ./libsvm/svm-train -t 1 -d 1 -c 20 -v 5 libsvm_cat_train.dat.scale
-# ./libsvm/svm-train -t 1 -d 1 -c 20 libsvm_cat_train.dat.scale
-# ./libsvm/svm-predict libsvm_cat_test.dat.scale libsvm_cat_train.dat.scale.model out
